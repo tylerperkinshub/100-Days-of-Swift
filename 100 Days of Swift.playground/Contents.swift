55c696e98081c1cb4147ae3f1bab2c762764b32e
Tyler Perkins <tylerperkins@gmail.com>
2019-04-30 01:11:06 -0400

    Add files via upload

---
+++ 100 Days of Swift.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+import Foundation
+
+
+/// Day 1
+
+//Strings
+var str = "Hello, playground"
+// Chaning value of declared string
+str = "Goobye"
+
+// Ints & Int with _ to format for easy reading
+var age = 38
+var population = 8_000_000
+
+
+//String over multiple line declaration
+var stringOverMultipleLines = """
+This goes
+over multiple
+lines
+"""
+//To make multiple lines appear on the same
+var stringOverMultipleLines1 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Double
+var pi = 3.141
+
+// Bool
+var awesome = true
+
+// String interpolation
+var score = 85
+var stringInterpolation = "Your score was \(score)"
+
+var results = "The test resulter are here: \(score)"
+
+// Constants
+let taylor = "swift"
+
+// Type Annotations
+let album: String = "Reputaion"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
+/// Day 2 
+
+
+
